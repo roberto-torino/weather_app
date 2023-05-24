6ad29a8379d05fd196efea045c7dafadb81c0d26
venir.dev <luca@venir.dev>
2023-05-24 17:45:28 +0200

    Implemented our Dio client

---
+++ lib/client/dio.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dio.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$httpClientHash() => r'7de24bfa08664201d2a843bc82a4dc91a59e24c3';
+
+/// See also [httpClient].
+@ProviderFor(httpClient)
+final httpClientProvider = AutoDisposeProvider<Dio>.internal(
+  httpClient,
+  name: r'httpClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$httpClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HttpClientRef = AutoDisposeProviderRef<Dio>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
